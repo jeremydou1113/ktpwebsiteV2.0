34a55ad8d117fdb7272d6c46c85d6d288e27ba06
Chinmay Savanur <csavanur@umich.edu>
2020-06-23 16:13:28 -0400

    got into for loop

--- assets/js/members.js
+++ assets/js/members.js
@@ -1,41 +1,41 @@
 
 var file = "\\assets\\Member List Fall 2020 - Sheet1.csv";
 
-document.getElementById("memberRow").innerHTML = file;
-
   var reader = new FileReader();
-  reader.readAsText(file);
-
-  //if you need to read a csv file with a 'ISO-8859-1' encoding
-  /*reader.readAsText(file,'ISO-8859-1');*/
+  document.getElementById("memberRow").innerHTML += "<p> Reader </p>";
 
   //When the file finish load
-  reader.onload = function(event) {
+  reader.onload = function(reader) {
+    document.getElementById("memberRow").innerHTML += "<p> Onload </p>";
+      //get the file.
+      var csv = toString(reader.result);
+      document.getElementById("memberRow").innerHTML += "<p> CSV " + typeof csv + "</p>";
+      //split and get the rows in an array
+      var rows = csv.split('\n');
+      document.getElementById("memberRow").innerHTML += "<p> Rows " + rows.length + "</p>";
+      //move line by line
+      for (var i = 1; i < 5; i++) {
+        document.getElementById("memberRow").innerHTML += "<p> Inside For </p>";
+        //split by separator (,) and get the columns
+        cols = rows[i].split(',');
 
-    //get the file.
-    var csv = event.target.result;
+        var memberName = cols[0] + " " + cols[1];
+        var memberImage = cols[5];
+        var memberPledgeClass = cols[6];
+        var memberLinkedIn = cols[8];
 
-    //split and get the rows in an array
-    var rows = csv.split('\n');
+        document.getElementById("memberRow").innerHTML += "<p> Hello World </p>";
+        /*
+        "<div class=\"active-item\"> <div class=\"active-img\"> <figure> <div class=\"member-img\"> <img src=" + memberImage + "> <div class=\"overlay\"> <div class=\"text\">& " + memberPledgeClass + ";</div> <a href= " + memberLinkedIn + "class=\"icon\" title=\"LinkedIn\"> <ion-icon name=\"logo-linkedin\" size=\"large\"></ion-icon> </a> </div> </div> <figcaption> " + memberName + "</figcaption> </figure> </div> </div>";
+        //move column by column
+        /*for (var j = 0; j < cols.length; j++) {
+          //the value of the current column.
+          //Do whatever you want with the value
+          var value = cols[j];
+        }*/
+      }
 
-    //move line by line
-    for (var i = 1; i < rows.length; i++) {
-      //split by separator (,) and get the columns
-      cols = rows[i].split(',');
 
-      var memberName = cols[0] + " " + cols[1];
-      var memberImage = cols[5];
-      var memberPledgeClass = cols[6];
-      var memberLinkedIn = cols[8];
+  })(reader);
 
-      document.getElementById("memberRow").innerHTML += "<p> Hello World </p>";
-      /*
-      "<div class=\"active-item\"> <div class=\"active-img\"> <figure> <div class=\"member-img\"> <img src=" + memberImage + "> <div class=\"overlay\"> <div class=\"text\">& " + memberPledgeClass + ";</div> <a href= " + memberLinkedIn + "class=\"icon\" title=\"LinkedIn\"> <ion-icon name=\"logo-linkedin\" size=\"large\"></ion-icon> </a> </div> </div> <figcaption> " + memberName + "</figcaption> </figure> </div> </div>";
-      //move column by column
-      /*for (var j = 0; j < cols.length; j++) {
-        //the value of the current column.
-        //Do whatever you want with the value
-        var value = cols[j];
-      }*/
-    }
-  }
+  reader.readAsText(file.files[0]);
